0b91d08a6e7156323f430bb976dd9fe838cf33ff
Jefferson Neves <jefferson.rpn@gmail.com>
2016-07-07 11:30:34 -0300

    Add angular-auto-validate

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@
  */
 angular
   .module('diferentonasStaffApp', [
-    'ngRoute'
+    'ngRoute', 'jcs-autoValidate'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -18,11 +18,6 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
